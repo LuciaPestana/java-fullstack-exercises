d95ce07bca2dc0ee2950fbba5ed0e760e4f074e6
LuciaPestana <luciapestana@hotmail.es>
2026-04-05 12:23:31 +0100

    feat: Se añade conversiones de primitivos a String

--- src/seccion02_variables/ConversionDeTipos.java
+++ src/seccion02_variables/ConversionDeTipos.java
@@ -15,9 +15,24 @@ public class ConversionDeTipos {
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
 
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
 
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
 
+        otroNumeroStr = String.valueOf(otroNumeroInt+10);
+        // Al ser dos Int puedes usar operador + y te suma los números, si usas el operador + con String, concatenará los valores
 
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.23456e2;
+
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
 
 
 
