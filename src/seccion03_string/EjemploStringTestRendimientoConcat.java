4c1a7e9e71f604e8a8065fc84b416207d65e6652
LuciaPestana <luciapestana@hotmail.es>
2026-04-05 17:33:12 +0100

    feat:Test de rendimiento String Concatenado

---
+++ src/seccion03_string/EjemploStringTestRendimientoConcat.java
@@ -0,0 +1,29 @@
+package seccion03_string;
+
+public class EjemploStringTestRendimientoConcat
+{
+    public static void main(String[] args) {
+
+        String a = "a";
+        String b = "b";
+        String c = a;
+        StringBuilder sb = new StringBuilder(a);
+        long inicio = System.currentTimeMillis();
+
+        for (int i = 0; i < 100000; i++) {
+
+            c = c.concat(a).concat(b).concat("\n"); // 500 => 1ms, 1000=> 1ms; 100000=>4538
+            // c += a + b + "\n"; // 500=> 3ms 1000=> 4ms, 100000 => 1604ms
+           // sb.append(a).append(b).append("\n"); // 500 => 0ms, 1000=> 0ms, 100000=> 6ms
+
+
+        }
+
+        long fin = System.currentTimeMillis();
+        System.out.println(fin- inicio);
+        System.out.println("c = " + c);
+        System.out.println("sb = " + sb);
+
+
+    }
+}
