f71046da0b13147dc5487e885bece9d40203736a
LuciaPestana <luciapestana@hotmail.es>
2026-04-07 15:51:50 +0100

    Login con Arrays

--- src/seccion04_operadores/OperadoresLogicosLoginConArreglos.java
+++ src/seccion04_operadores/OperadoresLogicosLoginConArreglos.java
@@ -1,15 +1,19 @@
 package seccion04_operadores;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class OperadoresLogicosLogin {
+public class OperadoresLogicosLoginConArreglos {
     public static void main(String[] args) {
 
-        String usuario = "Lucia";
-        String password = "12345";
 
-        String usuario2 = "admin";
-        String password2= "12345";
+        String [] usarnames = new String[2];
+        String [] passwords = new String[2];
+        usarnames [0]= "Lucia";
+        passwords [0]= "12345";
+
+        usarnames [1] = "admin";
+        passwords [1]= "12345";
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,13 +24,11 @@ public class OperadoresLogicosLogin {
         String p = scanner.next();
 
         boolean esAutenticado = false;
-
-        if( (usuario.equals(u) && password.equals(p)) ||
-                (usuario2.equals(u) && password2.equals(p)) ){
-                 esAutenticado = true;
-        } else {
-            esAutenticado = false;
-            System.out.println("Login incorrecto ");
+        for (int i = 0; i< usarnames.length; i ++){
+        if( (usarnames[i].equals(u) && passwords[i].equals(p)) ){
+                esAutenticado = true;
+                break;
+        }
 
         }if(esAutenticado){
             System.out.println("Bienvenido usario " .concat(u).concat(" !"));
@@ -39,5 +41,5 @@ public class OperadoresLogicosLogin {
         }
 
     }
-
+    
 }
