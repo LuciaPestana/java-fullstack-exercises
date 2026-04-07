95f37922a116c9409e640ac9185fa9f79883619a
LuciaPestana <luciapestana@hotmail.es>
2026-04-07 10:58:15 +0100

    feat: Versión de ejercicios con Scanner

--- src/tareas/ProgramaManejoDeNombresConScanner.java
+++ src/tareas/ProgramaManejoDeNombresConScanner.java
@@ -1,8 +1,8 @@
 package tareas;
 
 import java.util.Scanner;
-import java.util.Arrays;
-public class ProgramaManejoDeNombres {
+
+public class ProgramaManejoDeNombresConScanner {
 
     public static void main(String[] args) {
 
